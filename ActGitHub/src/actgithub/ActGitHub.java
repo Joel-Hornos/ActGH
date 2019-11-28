ac0b67b6ea506a5da2b7990ba4dd7f7f5ce934e0
Joel Hornos <jhorcas32@insgabrielamistral.cat>
2019-11-28 19:25:24 +0100

    Funcion

--- ActGitHub/src/actgithub/ActGitHub.java
+++ ActGitHub/src/actgithub/ActGitHub.java
@@ -7,33 +7,26 @@ package actgithub;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  *
  * @author Joel
  */
 public class ActGitHub {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-       {
-      InputStreamReader isr = new InputStreamReader(System.in);
-      BufferedReader br = new BufferedReader (isr);
-      try
-      {
-         System.out.print("Insereix el primer numero : ");
-         int n1 = Integer.parseInt(br.readLine());
-         System.out.print("Insereix el segon numero : ");
-         int n2 = Integer.parseInt(br.readLine());
-         int suma=n1+n2;
-         System.out.println ("La suma es " + n1 + "+" + n2 +"="+ suma);
-      }
-      catch (Exception e)
-      {
-      }
-   }
-}
+        Scanner sc = new Scanner(System.in);
+        int numero1, numero2, resultado;
+        System.out.print("Introduce primer número: ");
+        numero1 = sc.nextInt();
+        System.out.print("Introduce segundo número: ");
+        numero2 = sc.nextInt();
+        resultado = sumar(numero1, numero2);
+        System.out.println("Suma: " + resultado);
+    }
+    public static int sumar(int a, int b){
+           int c;
+           c = a + b;
+           return c;
+    }
 }
